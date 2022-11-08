ed86e1155602c8c6799f75408b7963ba1605ef9a
Viktor Persson <viktor.persson@arcsin.se>
2022-11-08 15:38:27 +0100

    Update link to google fonts.

--- src/components/GoogleFontPageLink.vue
+++ src/components/GoogleFontPageLink.vue
@@ -10,9 +10,9 @@ const fontNameToSlug = name => name.replaceAll(" ", "+");
 <template>
   <a
       :href="`https://fonts.google.com/specimen/${fontNameToSlug(fontFamily)}`"
-      class="text-gray-600 text-sm"
+      class="text-gray-600 text-xs"
   >
-    View on Google Fonts &#8599;
+    <strong>{{ fontFamily }}</strong> on Google Fonts &#8599;
   </a>
 </template>
 
