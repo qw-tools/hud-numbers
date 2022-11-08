30fe0ef9a81ffef57afadf625e32b5e88cfd021a
Viktor Persson <viktor.persson@arcsin.se>
2022-11-08 15:33:28 +0100

    Update fonts.

--- src/store.js
+++ src/store.js
@@ -1,11 +1,22 @@
 import { reactive } from "vue";
 
-const defaultGlyphSize = 80;
+const defaultGlyphSize = 128;
+const fontNames = [
+  "Alfa Slab One",
+  "Bungee",
+  "Bungee Inline",
+  "Lato",
+  "Londrina Shadow",
+  "Righteous",
+  "Ultra",
+  "Zen Dots",
+];
 
 export const store = reactive({
-  fontName: "Roboto",
+  fontNames,
   glyphContainerSize: "64px",
   glyphs: {
+    fontFamily: fontNames[0],
     size: defaultGlyphSize,
   },
   numColor: "#000000",
