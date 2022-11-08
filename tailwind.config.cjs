a48b3b5e2592dcff4f06656a77c041397f914bfd
Viktor Persson <viktor.persson@arcsin.se>
2022-11-08 12:50:11 +0100

    Add package, config.s

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
