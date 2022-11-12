d1435cc0eaa2e8166bee7c34fc2a458e4d076362
Viktor Persson <viktor.persson@arcsin.se>
2022-11-12 14:07:05 +0100

    Refactor helpers.

---
+++ src/components/Editor/Helpers.vue
@@ -0,0 +1,20 @@
+<script setup>
+import CenterCrosshair from "./Settings/CenterCrosshair.vue";
+import PreviewBackground from "./Settings/PreviewBackground.vue";
+</script>
+<template>
+  <div>
+    <div class="flex items-center space-x-12 mx-4">
+      <CenterCrosshair />
+      <PreviewBackground />
+    </div>
+    <div>
+      <div
+        class="border-x border-b h-2 border-gray-400 border-dashed mt-2"
+      ></div>
+      <div class="text-xs text-center text-gray-600 mt-2">
+        Not included in download
+      </div>
+    </div>
+  </div>
+</template>
