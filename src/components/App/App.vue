dea0aede406937dc1eea4c71f2fe9637d4437207
Viktor Persson <viktor.persson@arcsin.se>
2022-11-14 23:08:55 +0100

    Refactor app.

---
+++ src/components/App/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from "./Header.vue";
+import Footer from "./Footer.vue";
+import HudNumbers from "../HudNumbers/HudNumbers.vue";
+</script>
+
+<template>
+  <Header />
+  <HudNumbers />
+  <Footer />
+</template>
